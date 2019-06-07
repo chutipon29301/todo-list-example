cf7ce7cbf77a7dd77b58e33a2ec9a095b75cb654
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-07 10:07:45 +0700

    :sparkles: import util function

--- src/routers/todo/todo.service.ts
+++ src/routers/todo/todo.service.ts
@@ -1,4 +1,6 @@
-import { Mongoose } from 'mongoose';
+import { Model } from 'mongoose';
+import { Todo } from '../../models/todo.model';
+import { partialOf } from '../utils/objectMapper';
 
 export default {
 };
