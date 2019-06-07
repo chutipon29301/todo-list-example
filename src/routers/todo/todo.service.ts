aa0ae1da3e86b4d7f6f5eff8fe6bf82975b8dc65
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-07 10:08:06 +0700

    :sparkles: Add interface for creating todo document

--- src/routers/todo/todo.service.ts
+++ src/routers/todo/todo.service.ts
@@ -2,5 +2,10 @@ import { Model } from 'mongoose';
 import { Todo } from '../../models/todo.model';
 import { partialOf } from '../utils/objectMapper';
 
+export interface CreateTodo {
+    topic: string;
+    description: string;
+    dueDate: Date;
+}
 export default {
 };
