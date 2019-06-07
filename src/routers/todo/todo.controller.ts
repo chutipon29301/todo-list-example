ecc34c1fe9e38cc1d63ad0728b63cc87947da3cf
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-06-07 10:09:42 +0700

    :sparkles: import model and service

--- src/routers/todo/todo.controller.ts
+++ src/routers/todo/todo.controller.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 import { asyncifyHandler } from '../utils/async-handler';
+import { param, body, oneOf } from 'express-validator/check';
+import { validateRequest } from '../utils/requestValidator';
+import todoService from './todo.service';
+import { todo } from '../../models/todo.model';
 
 const router: Router = Router();
 
